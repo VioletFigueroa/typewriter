3127d4693ea6690d1a50999e69874c30451eaf7a
VioletFigueroa <Permafry_42@hotmail.com>
2021-03-10 02:24:38 +0000

    typewrite.js created

---
+++ typewriter.js
@@ -0,0 +1,10 @@
+const sentence = "hello there from lighthouse labs";
+let i = 0;
+for (const char of sentence) {
+  
+  setTimeout(() => {
+    process.stdout.write (char);
+  }, (50 * i));
+  i++;
+};
+setTimeout(() => process.stdout.write(`\n`), 50 * sentence.length); 
